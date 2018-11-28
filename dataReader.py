48e3dd3010341922a830464c2d7ff5d8ab2dfda5
Amr Khalifa <killua96@outlook.com>
2018-11-28 03:14:44 +0200

    finished feature generator code, before clustering

--- dataReader.py
+++ dataReader.py
@@ -2,15 +2,8 @@ import pandas as pd
 import numpy as np 
 
 
-def read_data():
-    df = pd.read_csv('customized_daily_rainfall_data.csv')
-    df.replace('?',-99999,inplace = True)
-    # columns 0:5 features, column 5 : prediction 
-    x= df[df.columns[0:5]].values
-    y= df[df.columns[5:6]].values
-    
-    return x,y
 
+x = np.array([1,2,3]).reshape(1,-1)
+df = pd.DataFrame(x)
 
-x,y = read_data()
-
+print(df.head())
--- feature_extractor.py
+++ feature_extractor.py
@@ -1,34 +1,91 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_data():
-    df = pd.read_csv('cleaned_customized_daily_rainfall_data .csv')
+    """ this funcitons reads the dataset file and returns a Pandas data frame the contains the data
 
-    df.replace('?',-99999,inplace = True)
+    Args:
+            NONE
+    Returns:
+        Pandas dataframe: dataframe the contains the data
+
+    """
+    df = pd.read_csv('cleaned_customized_daily_rainfall_data .csv')
+    df.replace('?', -99999, inplace=True)
     # columns 0:5 features, column 5 : prediction
     return df
 
 
-def get_month_data(df):
+def get_month_data(df, i):
+    """ This function gets the dataframe for a specific month
 
-    df = df.loc [df['StationIndex'] == 1]
-    df = df.loc[df['Month'] == 1]
+    Args:
+           df : Entire dataset dataframe ,
+           i : index of the month of interest (number)
+    Returns:
+        Pandas dataframe: dataframe the contains the data of the given month
 
-    return df
+    """
+    frame = df.loc[df['Month'] == i]
+    return frame
+
+
+def get_stats(df):
+    means = []
+    stds = []
+
+    for i in range(1, 13):
+        frame = get_month_data(df, i)
+        means.append(frame["Rainfall"].mean())
+        stds.append(frame["Rainfall"].std())
+
+    return np.array(means), np.array(stds)
+
+
+def get_feature_vector(df):
+    means, stds = get_stats(df)
+    means = means / abs(means).max()
+    stds = stds / abs(stds).max()
+
+    return np.concatenate((means, stds)).reshape(1, -1)
+
+
+def define_data_frame():
+    dataFrame = pd.DataFrame(np.zeros((1, 24)))
+
+    return dataFrame
+
+
+def generate_iteration_data_frame(feature_vector):
+    iteration_dataFrame = pd.DataFrame(feature_vector)
+    return iteration_dataFrame
+
+
+def concat_frames(f1, f2):
+    # frames = [f1, f2]
+    # frame = pd.concat(frames)
+    frame = f1.append(f2)
+    return frame
+
+
+def save_dataframe2CSV(f1, file):
+    f1.to_csv(file)
+
 
 dataframe = read_data()
 
-monthdata = get_month_data(dataframe)
-print(monthdata.head())
-print(monthdata.tail())
+cluster_input = define_data_frame()
 
-mean = monthdata["Rainfall"].mean()
-std = monthdata["Rainfall"].std()
-print(mean)
-print(std)
+for i in range(1, 36):
+    df = dataframe.loc[dataframe['StationIndex'] == i]
+    z = get_feature_vector(df)
 
+    station_dataframe = generate_iteration_data_frame(z)
+    cluster_input = concat_frames(cluster_input, station_dataframe)
 
+save_dataframe2CSV(cluster_input, "cluster_dataset.csv")
 
-
-
+my_frame = pd.read_csv('cluster_dataset.csv')
+print(my_frame.values)
