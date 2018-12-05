e6ab619145d8d61c3490d7cee949c5c1e11b628c
James Assiene <assiene.james@mail.com>
2018-12-05 21:32:20 +0200

    Added the latest versions of preprocessing and tutorials

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -16,11 +16,11 @@ class DataPreprocessing:
     def __init__(self):
         pass
     
-    def create_dataset_without_tai(self, entire_dataframe, past_timeframe=20, stock_price="Close", preprocess=True):
+    def create_dataset_in_time_series_form(self, entire_dataframe, time_horizon=20, time_series_column="Rainfall", output_form="sklearn", preprocess=False):
         start_index = 0
-        end_index = past_timeframe
+        end_index = time_horizon
         entire_dataframe_rows_number = len(entire_dataframe)
-        rising_column_name = "Rising"
+        rising_column_name = "Y_t_plus_1"
         X = []
         Y = pd.DataFrame(columns=[rising_column_name])
         
@@ -30,9 +30,13 @@ class DataPreprocessing:
                 X.append(preprocessing.scale(df_subset.values))
             else:
                 X.append(df_subset.values)
-            is_price_rising =  1 if (entire_dataframe[stock_price].iloc[end_index] > entire_dataframe[stock_price].iloc[end_index - 1]) else 0
-            Y = Y.append([{rising_column_name: is_price_rising}])
+            Y = Y.append([{rising_column_name: entire_dataframe[time_series_column].iloc[end_index]}])
             start_index+= 1
             end_index+= 1
             
-        return np.array(X),Y
+        X = np.array(X)
+            
+        if output_form == "sklearn":
+            X = np.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]))
+            
+        return X, Y
